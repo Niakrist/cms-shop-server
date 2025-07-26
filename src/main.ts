fbd3990692dc3642d12442fb87e5d0b9148ad04f
Niakrist <ak_88dron@inbox.ru>
2025-07-26 15:55:21 +0300

    feat: add ConfigModule, cookieParser and enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser()); // cookie-parser
+  app.enableCors({
+    origin: [process.env.CLIENT_URL], // Порт на который можно отправлять запросы
+    credentils: true, // Так как работаем с серверными cookie
+    exposedHeaders: 'set-cookie', // добавляет заголовок set-cookie для CORS
+  });
+
   await app.listen(process.env.PORT || 5001);
 }
 bootstrap();
